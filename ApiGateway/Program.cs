c10b4c584ba549328e539d33cbf61d16e49057af
MozzarellaCheesee <Danilvasilkov59@gmail.com>
2025-10-16 21:11:41 +0300

    gitignore file added

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -5,13 +5,11 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем Ocelot
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-// Настраиваем middleware Ocelot
 app.UseOcelot().Wait();
 
 app.Run();
